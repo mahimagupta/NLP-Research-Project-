9451f608db5f8b1249731ee4ade98a48f5f822c1
Mahima Gupta <mahimagupta2795@gmail.com>
2019-11-28 15:45:07 -0800

    Preprocessing

---
+++ Final Implementation/processModel.py
@@ -0,0 +1,39 @@
+
+# -*- coding: utf-8 -*-
+inputFile = open('test.conllu','r')
+# outputFile = open('t.txt','w')
+# outputFile.write('<s> START')
+# outputFile.write('\n')
+ruleFile = open('rules.txt','w')
+prev =['None']
+lineArray = []
+for line in inputFile:
+    
+    if line.find('#')!=-1:
+        continue
+    elif line.strip('\n')!='':
+
+       
+        line = line.split()
+
+        if line[1]=='।':
+            prev = ['None']
+
+        elif line[4]=='SYM' or any(char.isdigit() for char in line[1]):
+            prevTag = prev.pop(0)
+            currTag = line[4]
+            prev.append(currTag)
+        else:
+            prevTag = prev.pop(0)
+            element = line[1] + ' ' + line[4] + ' ' +prevTag +' '+ line[2]
+            currTag = line[4]
+            prev.append(currTag)
+            
+            if element in lineArray:
+                continue
+            lineArray.append(element)
+            ruleFile.write(element)
+            ruleFile.write('\n')
+            
+            
+        
